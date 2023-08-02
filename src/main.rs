27b0913fa79ca982c1db239f4d80ac9c4d36303c
carrotshniper21 <vipershniper21@gmail.com>
2023-08-02 10:01:15 -0500

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,18 @@
-use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    response::IntoResponse,
-    Json, Router,
-};
-use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
-use consumet_api_rs::{providers::movies, models::IMovieResult};
-use consumet_api_rs::models::ISearch;
+use consumet_api_rs::providers::movies;
+
+#[derive(Debug)]
+struct Test {
+    id: String,
+}
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
-
-    // build our application with a route
-    let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root))
-        .route("/flixhq", get(flixhq_search));
-
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
-
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
-}
-
-async fn flixhq_search() -> impl IntoResponse {
     let flixhq = movies::FlixHQ;
 
-    let data = flixhq.search("hi", None).await.unwrap();
-    (StatusCode::OK, Json(data))
+    let deez = flixhq.search("hi", None).await.unwrap();
+    let result = Test {
+        id: deez.results[0].id.clone().unwrap(),
+    };
+
+    println!("{:#?}", result);
 }
