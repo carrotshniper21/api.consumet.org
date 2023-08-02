b70eac25571991097ff84c7030245cfa838cf4e4
carrotshniper21 <vipershniper21@gmail.com>
2023-08-02 09:46:26 -0500

    Fun time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    response::IntoResponse,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use consumet_api_rs::{providers::movies, models::IMovieResult};
+use consumet_api_rs::models::ISearch;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        .route("/flixhq", get(flixhq_search));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn flixhq_search() -> impl IntoResponse {
+    let flixhq = movies::FlixHQ;
+
+    let data = flixhq.search("hi", None).await.unwrap();
+    (StatusCode::OK, Json(data))
 }
